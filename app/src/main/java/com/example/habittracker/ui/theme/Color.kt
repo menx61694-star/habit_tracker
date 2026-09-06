5ed6993ab8ad4e448c3c5e38344289da96b0a8a8
PIXEL FORGE <menx61694@gmail.com>
2026-09-06 19:38:12 +0530

    Refine Habit Tracker color palette

--- app/src/main/java/com/example/habittracker/ui/theme/Color.kt
+++ app/src/main/java/com/example/habittracker/ui/theme/Color.kt
@@ -8,3 +8,13 @@ val HabitBlue = Color(0xFF4267A9)
 val HabitBlueLight = Color(0xFFAFC8FF)
 val HabitOrange = Color(0xFFB86A25)
 val HabitOrangeLight = Color(0xFFFFB978)
+
+val HabitBackgroundLight = Color(0xFFF7F8F6)
+val HabitSurfaceLight = Color(0xFFFFFFFF)
+val HabitSurfaceVariantLight = Color(0xFFE8EEE9)
+val HabitOnSurfaceVariantLight = Color(0xFF4D5751)
+
+val HabitBackgroundDark = Color(0xFF111412)
+val HabitSurfaceDark = Color(0xFF191D1A)
+val HabitSurfaceVariantDark = Color(0xFF29302B)
+val HabitOnSurfaceVariantDark = Color(0xFFCBD4CE)
