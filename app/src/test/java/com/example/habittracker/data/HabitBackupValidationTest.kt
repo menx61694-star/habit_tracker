c2c70db519cc1966ef4ac8311520c3e94bed5c88
PIXEL FORGE <menx61694@gmail.com>
2026-09-08 23:20:41 +0530

    Point backup tests to instrumentation suite

--- app/src/test/java/com/example/habittracker/data/HabitBackupValidationTest.kt
+++ app/src/test/java/com/example/habittracker/data/HabitBackupValidationTest.kt
@@ -1,3 +1,3 @@
 package com.example.habittracker.data
 
-// Instrumentation suite is located under app/src/androidTest.
+// See app/src/androidTest/java/com/example/habittracker/data/HabitBackupValidationTest.kt
